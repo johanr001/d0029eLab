83db8fede81b6076a10a2587c486de8159b7721f
Johan Risberg <johan.s@risberg.com>
2025-09-17 17:46:35 +0200

    Added some more things

--- Lab2/rsa.py
+++ Lab2/rsa.py
@@ -9,6 +9,7 @@ def egcd(x, y):
         g, h, i = egcd(y % x, x)
         return (g, i - (y // x) * h, h)
 
+#Function to do modular inverse
 def modInv(e, p):
     g, h, i = egcd(e, p)
     if g != 1:
@@ -30,23 +31,27 @@ def generate_rsa_key(p, q, e):
     d = modInv(e, tot_n_ham)
 
     #Return the public key (e, n) and the private key d
-    return e, n, d
+
+    pub_k = (e, n)
+
+    return pub_k, d
+
+
 
 def main():
 
     #Test of keygen. Result of this should be: e=7, n=187, d=23
-    #p = 17
-    #q = 11
-    #e = 7
+    p = 17
+    q = 11
+    e = 7
 
     #Other test
 
-    p=int('F7E75FDC469067FFDC4E847C51F452DF', 16)
+    #p=int('F7E75FDC469067FFDC4E847C51F452DF', 16)
 
-    q=int('E85CED54AF57E53E092113E62F436F4F', 16)
+    #q=int('E85CED54AF57E53E092113E62F436F4F', 16)
 
-    e=int('0D88C3', 16)
-
+    #e=int('0D88C3', 16)
 
     print(generate_rsa_key(p, q, e))
 
