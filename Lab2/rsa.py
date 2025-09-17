913b7936bb8f0ccd061b2a71e7d6adbe32168de0
johanr001 <johris-3@student.ltu.se>
2025-09-17 21:26:03 +0200

    Decryption now works, yay!

--- Lab2/rsa.py
+++ Lab2/rsa.py
@@ -33,25 +33,42 @@ def generate_rsa_key(p, q, e):
     #e = math.gcd(tot_n_ham, e)
     
     #Determine d
-    d = hex(getModInv(e, tot_n_ham))
+    d = getModInv(e, tot_n_ham)
 
-    #Return the public key (e, n) and the private key d
+    #Return the public key (e, n) and the private key (d, n)
 
     pub_k = (hex(e), hex(n))
 
-    return pub_k, d
+    pri_k= (hex(d), hex(n))
+
+    return pub_k, pri_k
 
 def rsa_encrypt(message, public_key):
+    #M = message , Only needed for testing
 
-    M = int.from_bytes(message.encode(), 'big')
-
+    #Conversion to int
+    M = int.from_bytes(message.encode(), 'big')   
     e=int(public_key[0], 16)
-
     n=int(public_key[1], 16)
 
-    C = pow(M, e) % n
+    C = pow(M, e, n)
 
     return hex(C)
+
+def rsa_decrypt(ciphertext, private_key):
+
+    #Conversion to int
+    C = int(ciphertext, 16)
+    d = int(private_key[0], 16)
+    n = int(private_key[1], 16)
+
+    M = hex(pow(C, d, n)).replace(' ', '').replace('0x', '').replace('\t', '').replace('\n', '')
+    print(M)
+
+    #Convert plaintext to ascii string and return
+    pt = bytes.fromhex(M).decode("utf-8")
+    return pt
+
 
 
 def main():
@@ -63,25 +80,35 @@ def main():
 
     #Other test
 
-    p='F7E75FDC469067FFDC4E847C51F452DF'
+    #p='F7E75FDC469067FFDC4E847C51F452DF'
 
-    q='E85CED54AF57E53E092113E62F436F4F'
+    #q='E85CED54AF57E53E092113E62F436F4F'
 
-    e='0D88C3'
+    #e='0D88C3'
 
-    print(generate_rsa_key(p, q, e))
+    #print(generate_rsa_key(p, q, e))
 
     #Test of encryption
 
     msg = "A top secret!"
-
     e2='010001'
+    n2='DCBFFE3E51F62E09CE7032E2677A78946A849DC4CDDE3A4D0CB81629242FB1A5'
 
-    n='DCBFFE3E51F62E09CE7032E2677A78946A849DC4CDDE3A4D0CB81629242FB1A5'
+    #Other test, Result of this should be: C=11
+    #msg=88
+    #e2=hex(7)
+    #n=hex(187)
 
-    pub_k = (e2, n)
+    #Test of decryption
 
-    print(rsa_encrypt(msg, pub_k))
+    pub_k = (e2, n2)
+    #print(rsa_encrypt(msg, pub_k))
+    ct = '8C0F971DF2F3672B28811407E2DABBE1DA0FEBBBDFC7DCB67396567EA1E2493F'
+    
+    d2 = '74D806F9F3A62BAE331FFE3F0A68AFE35B3D2E4794148AACBC26AA381CD7D30D'
+    pri_k = (d2, n2)
+
+    print(rsa_decrypt(ct, pri_k))
 
 
 
