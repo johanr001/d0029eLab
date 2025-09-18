8c39544ee2a878e6e79c4b061f3eff966c903c72
johanr001 <johris-3@student.ltu.se>
2025-09-18 17:14:13 +0200

    Some small fixes...

--- Lab2/rsa.py
+++ Lab2/rsa.py
@@ -11,15 +11,13 @@ def egcd(x, y):
 
 #Function to get modular inverse
 def getModInv(e, p):
-    g, h, i = egcd(e, p)
+    g, h, _ = egcd(e, p)
     if g != 1:
         raise Exception("No modular inverse exists")
     else:
         return h % p
 
-
 def generate_rsa_key(p, q, e):
-    
     #Conversion to int
     p=int(p, 16)
     q=int(q, 16)
@@ -33,34 +31,29 @@ def generate_rsa_key(p, q, e):
     d = getModInv(e, tot_n_ham)
 
     #Return the public key (e, n) and the private key (d, n)
-
     pub_k = (hex(e), hex(n))
-
     pri_k= (hex(d), hex(n))
-
     return pub_k, pri_k
 
 def rsa_encrypt(message, public_key):
-    #M = message , Only needed for one test with message 88
-
     #Conversion to int
     M = int.from_bytes(message.encode(), 'big')   
     e=int(public_key[0], 16)
     n=int(public_key[1], 16)
 
-    C = pow(M, e, n)
-
-    return hex(C)
+    #Determine ciphertext and return
+    C = hex(pow(M, e, n)).replace('0x', '')
+    return C
 
 def rsa_decrypt(ciphertext, private_key):
-
     #Conversion to int
     C = int(ciphertext, 16)
     d = int(private_key[0], 16)
     n = int(private_key[1], 16)
 
+    #Determine the plaintext M in hex
     M = hex(pow(C, d, n)).replace('0x', '')
-
+    
     #Convert plaintext to ascii string and return
     pt = bytes.fromhex(M).decode("utf-8")
     return pt
@@ -76,13 +69,13 @@ def main():
 
     #Other test
 
-    #p='F7E75FDC469067FFDC4E847C51F452DF'
+    p='F7E75FDC469067FFDC4E847C51F452DF'
 
-    #q='E85CED54AF57E53E092113E62F436F4F'
+    q='E85CED54AF57E53E092113E62F436F4F'
 
-    #e='0D88C3'
+    e='0D88C3'
 
-    #print(generate_rsa_key(p, q, e))
+    print(generate_rsa_key(p, q, e))
 
     #Test of encryption
 
@@ -91,16 +84,18 @@ def main():
     n2='DCBFFE3E51F62E09CE7032E2677A78946A849DC4CDDE3A4D0CB81629242FB1A5'
 
     #Other test, Result of this should be: C=11
+    #M = message , Only needed in encryption function for this one test
     #msg=88
     #e2=hex(7)
     #n=hex(187)
 
     #Test of decryption
-
     pub_k = (e2, n2)
-    #print(rsa_encrypt(msg, pub_k))
-    ct = '8C0F971DF2F3672B28811407E2DABBE1DA0FEBBBDFC7DCB67396567EA1E2493F'
+    print(rsa_encrypt(msg, pub_k))
+    #ct=rsa_encrypt(msg, pub_k)
+    #print(ct)
     
+    ct = '8C0F971DF2F3672B28811407E2DABBE1DA0FEBBBDFC7DCB67396567EA1E2493F'
     d2 = '74D806F9F3A62BAE331FFE3F0A68AFE35B3D2E4794148AACBC26AA381CD7D30D'
     pri_k = (d2, n2)
 
